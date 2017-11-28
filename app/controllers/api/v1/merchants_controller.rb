553805e6945809ca28f9aec4d503de048a1c5a4d
izaxveta <izavetha@gmail.com>
2017-11-27 23:27:00 -0700

    Created merchant show action in merchant controller.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: Merchant.all
   end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
