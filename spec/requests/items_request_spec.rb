bd71ffb6d353e7c50cc26b374a3330c8ea8b54aa
Aaron Schreck <schreck.aaron@gmail.com>
2017-11-30 09:31:17 -0700

    test wip

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -1,23 +1,25 @@
 require "rails_helper"
 
 describe "Invoices API" do
+  before(:each) do
+    @customer = create(:customer)
+    @merchant = create(:merchant)
+    @item     = create(:item, merchant: @merchant)
+    @invoice = create(:invoice_item, customer: @customer, merchant: @merchant)
+  end 
   it "sends a list of items" do
-    customer = create(:customer)
-    merchant = create(:merchant)
-    item     = create(:item, merchant: merchant)
-    
-
+    item_2 = create(:item, merchant: @merchant)
     get '/api/v1/items'
-
+    
+    items = JSON.parse(response.body)
     expect(response).to be_success
 
-    items = JSON.parse(response.body)
+    expect(items.count).to eq 2 
+
   end
 
   it 'can obtain one merchant by the :id' do
-    customer = build(:customer)
-    merchant = build(:merchant)
-    id = create(:item, merchant: merchant).id
+    id = create(:item, merchant: @merchant).id
 
     get "/api/v1/items/#{id}"
 
@@ -25,5 +27,11 @@ describe "Invoices API" do
 
     expect(response).to be_success
     expect(customer["id"]).to eq(id)
+  end
+
+  it "can return a collection of associated invoice items" do 
+    invoice_item_1 = create(:invoice_item, invoice: @invoice, item: @item)
+    invoice_item_2 = create(:invoice_item, invoice: @invoice, item: @item)
+
   end
 end
