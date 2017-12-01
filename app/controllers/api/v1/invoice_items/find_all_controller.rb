d01b953e9de470b8c91c8524a75d6b2f10627821
Aaron Schreck <schreck.aaron@gmail.com>
2017-11-30 18:23:20 -0700

    fix numbers

--- app/controllers/api/v1/invoice_items/find_all_controller.rb
+++ app/controllers/api/v1/invoice_items/find_all_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::InvoiceItems::FindAllController < ApplicationController
   def index
-    render json: InvoiceItem.find_all_invoice_items(invoice_item_params)
+    render json: InvoiceItem.where(unit_price: invoice_item_params[:unit_price])
   end
 
   private
 
   def invoice_item_params
+    params[:unit_price].delete!(".") if params[:unit_price]
     params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end
